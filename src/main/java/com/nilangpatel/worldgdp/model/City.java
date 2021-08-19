778a8748825749ec62b11975baebcdb94d1ba790
Junior Fungwa Moke <junior.fungwamoke.etu@univ-lille.fr>
2021-08-19 13:02:52 +0200

    feat(add validator):add  validator annotation to city entity

--- src/main/java/com/nilangpatel/worldgdp/model/City.java
+++ src/main/java/com/nilangpatel/worldgdp/model/City.java
@@ -1,5 +1,7 @@
 package com.nilangpatel.worldgdp.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,9 +10,21 @@ import lombok.Setter;
 @Getter
 @Setter
 public class City {
+    @NotNull
     private Long id;
+
+    @NotNull
+    @Size(max = 35)
     private String name;
+
+    @NotNull
+    @Size(max = 3, min = 3)
     private Country country;
+
+    @NotNull
+    @Size(max = 20)
     private String district;
+
+    @NotNull
     private Long population;
 }
